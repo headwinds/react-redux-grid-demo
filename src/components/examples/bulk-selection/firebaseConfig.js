0838a6e5f183ae4194b4a83fb88382d6dafe0a31
brandon flowers <brandon.flowers@qochealth.com>
2018-02-22 17:02:12 -0500

    added config so zeit knows about it

---
+++ src/components/examples/bulk-selection/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyCJtn_3SvM08FNNjP3u6axZZ5AzbMDuPVM',
+  authDomain: 'react-redux-grid.firebaseapp.com',
+  databaseURL: 'https://react-redux-grid.firebaseio.com',
+  projectId: 'react-redux-grid',
+  storageBucket: 'react-redux-grid.appspot.com',
+  messagingSenderId: '569836944248'
+};
